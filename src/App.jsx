fd174ce9357e6f06ba6e09e6e82a436f68dbdd79
Moumita Das <dasmoumita.me@gmail.com>
2023-05-27 12:51:48 +0530

    adding timmer and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,108 @@
 
 import {  useEffect, useState } from 'react'
 import './App.css'
+import { keyWord } from './Words'
 
-// let keyWord=()=> `asdfjkl;`.split(" ").sort(()=>Math.random()>0.5 ? 1: -1)
-// let keyWord=()=> `asdfjkl;  asdfjkl; asdfjkl;`
 
-// let keyWord=
-//   "asdfjkl; ,asdfjkd;s ,asddewfdwfjkl;"
-let keyWord="asdfjkl; , ;lkjfdsa , fdsajkl;, asdfjkl;, asdfjkl;, asdfjkl;, asdfjkl;, asdfjk;l ,asdf jk;,lasdfjkl; ,fdldska;,fjfkdls;,alskdfj aaassss,ddddffffjj,jkklll;;;;lkj, jkl;as"
 function App() {
   let [userInput,setUserInput]=useState('')
   let [words,setWords]=useState([])
+  let [min,setMin]=useState(0)
+  let [sec,setSec]=useState(0)
+  let [currWordIndex,setCurrWordIndex]=useState(0)
 
+  let [currCharIndex,setCurrCharIndex]=useState(-1)
+  const [currChar,setCurrChar]=useState("")
+
+  let [right,setRight]=useState(0)
+  let [wrong,setWrong]=useState(0)
+
+  // let textInput=useRef(null)
+  // const [status,setStatus]=useState("waiting")
+  
+
+  var timmer;
+  useEffect(()=>{
+    timmer=setInterval(()=>{
+setSec(sec+1)
+if(sec===59){
+  setMin(min+1)
+  setSec(0)
+}
+
+  },1000)
+  return ()=>clearInterval(timmer)
+})
   // let key=useRef(keyWord())
+
+
+
+
+let restart=()=>{
+  setMin(0)
+  setSec(0)
+}
+
+let stopp=()=>{
+  clearInterval(timmer)
+}
+
+
+let charClass=(wordIdx,charIdx,char)=>{
+  if(wordIdx===currWordIndex && charIdx===currCharIndex && currChar){
+    if(char ===currChar){
+      // return `background-color:"green"`
+      console.log("c",char)
+      console.log("cu",currChar)
+
+      // return green
+
+    }else{
+      // return `background-color:"red"`
+      // console.log("cc",char)
+      // console.log("ccu",currChar)
+      // alert("wrong key")
+      // ret
+
+    }
+  }
+
+}
 
   useEffect(()=>{
 setWords(randomWord)
   },[])
 
-  let randomWord=keyWord.split(",")
-// console.log(keyWord.split(","))
-//   return (
-//     <>
-//       <div>
+  let randomWord=keyWord.split(" ")
+
+let handleKeyDown=({keyCode,key})=>{
+  // console.log(e.key)
+  if(keyCode===32){
+    checkMatch()
+    setUserInput("")
+    setCurrWordIndex(currWordIndex+1)
+    setCurrCharIndex(-1)
+  }else{
+    setCurrCharIndex(currCharIndex+1)
+    setCurrChar(key)
+  }
+}
+
+let checkMatch=()=>{
+  const wordToCompare=words[currWordIndex]
+  const doesMatch= wordToCompare==userInput.trim()
+  // console.log({doesMatch})
+  // console.log("doesMatch")
+  if(doesMatch){
+    setRight(right+1)
+  }else{
+    setWrong(wrong+1)
+  }
+
+}
 
 
-// <div className='container'>
-// {/* {words.map((word,index)=>{
-//     return (
-//       <>
-//       <div key={index}>
 
-//         {word}{index !== words.length - 1 && ' '}
- 
-//         <p> </p>
-//         </div>
-//       </>
-//     )
-
-//   })} */}
-
-
-// </div>
-
-//  <input
-//   type='text'
-//   value={userInput}
-//   onChange={(e)=>setUserInput(e.target.value)}
-//  />
-//       </div>
-//     </>
-//   )
 
 return (
   <div  className='main'>
@@ -59,26 +111,51 @@ return (
 
   <div className='wordBox'>
     {words.map((element, index) => (
-      <span key={index} className='word'>
-        {element}{' '}
+    
+      <>
+      <span style={{border:"1px solid blue",margin:"5px"}} key={index} className='word'>
+        {element.split("").map((char,i)=>(
+          <span style={{margin:"5px"}} className={charClass(index,i,char)} key={i}>{char}</span>
+          
+        ))}
+        {/* {" "}  */}
+      
       </span>
+      <span> </span>
+      </>
     ))}
     </div>
     <input
   type='text'
   value={userInput}
   onChange={(e)=>setUserInput(e.target.value)}
+  onKeyDown={handleKeyDown}
  />
  <div>
+ <div>
+ {/* <div>
+  <h3>Word per minute</h3>
+  <h3>{right}</h3>
+</div> */}
+ <div>
+  <h3>Acuracy</h3>
+  {/* <h3>{NaN ? Math.round((right/(right+wrong))*100):"0"}%</h3> */}
+  <h3>{ Math.round((right/(right+wrong))*100)}%</h3>
 
- <button>Start</button>
- <button>Restart</button>
+</div>
+
+ </div>
+
+ {/* <button onClick={start}>Start</button> */}
+ <button onClick={restart}>Restart</button>
+ <button onClick={stopp}>Stop</button>
+
  </div>
   </div>
   <div className='timmerDiv'>
   <div>
   <h2>Timmer Clock</h2>
-  <h2>5</h2>
+  <h2>{min<10? "0"+min:min}:{sec<10? "0"+sec:sec}</h2>
 
   </div>
 
---
+++ src/Words.jsx
@@ -0,0 +1,2 @@
+export let keyWord=
+"asd asdfjkl; ;lkjfdsa fdsajkl; asdfjkl; asdfjkl; asdfjkl; asdfjkl; asdfjk;l asdf jk; lasdfjkl; fjfkdls; alskdfj aaassss ddddffffjj jkklll;;;;lkj jkl;as asdfjk;l asdf jk; lasdfjkl; fjfkdls; alskdfj aaassss ;lkjfdsa fdsajkl; asdfjkl; asdfjkl; asdfjkl; asdfjk;l asdf jk; lasdfjkl; fjfkdls; alskdfj aaassss ;lkjfdsa kljasds kljsdasd klkj;ksd kkjlljsds lkkdsak lkjasdasd"
